68c2df701c5ac79239e7311179fff27839bc95b0
FONSECA Fabio <fabio.fonseca@altran.com>
2018-12-14 10:23:21 +0000

    Initial Commit

---
+++ MacIconMaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacIconMaker
+//
+//  Created by Miguel Vieira on 14/12/2018.
+//  Copyright © 2018 Miguel Vieira. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacIconMaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacIconMaker
+//
+//  Created by Miguel Vieira on 14/12/2018.
+//  Copyright © 2018 Miguel Vieira. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
